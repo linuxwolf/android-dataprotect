5b583bd90675f97104505cc31d433ed3bca52434
Matthew A. Miller <linuxwolf@outer-planes.net>
2018-08-17 12:13:11 -0600

    switch from a Keychain-like to a Transcode-like model

---
+++ app/src/main/java/org/mozilla/mmiller/dataprotect/KeystoreAccess.kt
@@ -0,0 +1,105 @@
+package org.mozilla.mmiller.dataprotect
+
+import android.content.SharedPreferences
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Base64
+import android.util.Log
+import java.nio.charset.StandardCharsets
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.GCMParameterSpec
+
+private val KEYSTORE_PROVIDER = "AndroidKeyStore"
+private val KEYCHAIN_KEY = "keychain"
+private val B64_FLAGS = Base64.URL_SAFE or Base64.NO_PADDING or Base64.URL_SAFE
+private val CIPHER_SPEC = "AES/GCM/NoPadding"
+
+class KeystoreAccess {
+    enum class LockState {
+        LOCKED,
+        UNLOCKED
+    }
+
+    private val keystore : KeyStore
+    private val cache : MutableMap<String, SecretKey>
+    private var lockstate : LockState = LockState.LOCKED
+
+    init {
+        keystore = KeyStore.getInstance(KEYSTORE_PROVIDER)
+        keystore.load(null)
+
+        val initCache = HashMap<String, SecretKey>()
+        for (a in keystore.aliases()) {
+            try {
+                val key = keystore.getKey(a, null) as SecretKey
+                initCache[a] = key
+                Log.i("KeychainAccess", "keystore alias $a := $key")
+            } catch (ex : Exception) {
+                Log.d("KeychainAccess", "invalid entry $a")
+            }
+        }
+        cache = initCache
+    }
+
+    val locked : Boolean get() = lockstate == LockState.LOCKED
+    fun lock() {
+        lockstate = LockState.LOCKED
+    }
+    fun unlock() {
+        lockstate = LockState.UNLOCKED
+    }
+
+    fun available(label: String) = cache.contains(label)
+    fun generate(label: String) {
+        if (cache.contains(label)) {
+            return
+        }
+
+        val spec = KeyGenParameterSpec.Builder(label, KeyProperties.PURPOSE_DECRYPT or KeyProperties.PURPOSE_ENCRYPT)
+                .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                .build()
+        val keygen = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, KEYSTORE_PROVIDER)
+        keygen.init(spec)
+        val key = keygen.generateKey()
+        cache[label] = key
+    }
+
+    fun encryptBytes(label : String, pdata : ByteArray) : ByteArray {
+        val key = cache[label] ?: throw IllegalArgumentException("unknonwn label: $label")
+
+        // outputs what is called a "5116-style" crypto object:
+        //    <cipher inputs> || <cipher text + tag>
+        // The <cipher inputs> include the following here:
+        //    <version = 0x01> || <iv/nonce (12 bytes)>
+        val cipher = Cipher.getInstance(CIPHER_SPEC)
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+
+        val ver = 0x01
+        val iv = cipher.iv
+
+        val cdata = cipher.doFinal(pdata)
+        return byteArrayOf(ver.toByte(), *iv, *cdata)
+    }
+    fun decryptBytes(label : String, encrypted : ByteArray) : ByteArray {
+        val key = cache[label] ?: throw IllegalArgumentException("unknonwn label: $label")
+
+        // expects a "5116-style" crypto object from above
+        val ver = encrypted[0].toInt()
+        if (ver != 0x01) {
+            throw IllegalArgumentException("unsupported version: $ver")
+        }
+
+        val iv = encrypted.sliceArray(1..12)
+        val cdata = encrypted.sliceArray(13..encrypted.size-1)
+
+        val cipher = Cipher.getInstance(CIPHER_SPEC)
+        cipher.init(Cipher.DECRYPT_MODE, key, GCMParameterSpec(128, iv))
+        val pdata = cipher.doFinal(cdata)
+
+        return pdata
+    }
+}
--- app/src/main/java/org/mozilla/mmiller/dataprotect/MainActivity.kt
+++ app/src/main/java/org/mozilla/mmiller/dataprotect/MainActivity.kt
@@ -1,13 +1,118 @@
 package org.mozilla.mmiller.dataprotect
 
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.util.Base64
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import java.nio.charset.StandardCharsets
+
+class KeyValAdapter(
+        private val prefs: SharedPreferences,
+        private val keychain: KeystoreAccess,
+        private val itemKeys: List<String>
+) : RecyclerView.Adapter<KeyValAdapter.ViewHolder>() {
+    override fun getItemCount() = itemKeys.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.keyval_item, parent,  false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, pos: Int) {
+        val key = itemKeys[pos]
+        var value = prefs.getString(key, "")
+
+        if (!keychain.locked) {
+            var bvalue = Base64.decode(value, Base64.URL_SAFE or Base64.NO_WRAP or Base64.NO_PADDING)
+            bvalue = keychain.decryptBytes("keychain", bvalue)
+            value = String(bvalue, StandardCharsets.UTF_8)
+        }
+
+        holder.keyView.text = key
+        holder.valView.text = value
+    }
+
+    class ViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+        var keyView : TextView
+        var valView : TextView
+
+        init {
+            keyView = view.findViewById(R.id.key_view)
+            valView = view.findViewById(R.id.val_view)
+        }
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var keychain : KeystoreAccess
+    private val itemKeys: List<String>
+
+    init {
+        itemKeys = ArrayList<String>().apply {
+            for (idx in 1..5) {
+                add("key $idx")
+            }
+        }
+    }
+
+    private lateinit var keyvalView : RecyclerView
+    private lateinit var keyvalAdapter : KeyValAdapter
+    private lateinit var keyvalManager : RecyclerView.LayoutManager
+    private lateinit var btnToggleLocked : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        keychain = KeystoreAccess()
+        keychain.generate("keychain")
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this).apply {
+            val editor = this.edit()
+            for (kname in itemKeys) {
+                if (!this.contains(kname)) {
+                    val pvalue = "value for $kname"
+                    val encrypted = keychain.encryptBytes("keychain", pvalue.toByteArray(StandardCharsets.UTF_8))
+                    val kvalue = Base64.encodeToString(encrypted, Base64.NO_PADDING or Base64.NO_WRAP or Base64.URL_SAFE)
+                    editor.putString(kname, kvalue)
+                    Log.d("Main", "created pref '$kname'")
+                }
+            }
+            editor.commit()
+        }
+
+        keyvalAdapter = KeyValAdapter(prefs, keychain, itemKeys)
+        keyvalManager = LinearLayoutManager(this)
+
+        keyvalView = findViewById<RecyclerView>(R.id.keyval_list).apply {
+            setHasFixedSize(true)
+
+            layoutManager = keyvalManager
+            adapter = keyvalAdapter
+        }
+
+        btnToggleLocked = findViewById(R.id.btn_toggle_locked)
+        btnToggleLocked.setOnClickListener { onToggleLockedClicked() }
+    }
+
+    private fun onToggleLockedClicked() {
+        if (!keychain.locked) {
+            keychain.lock()
+        } else {
+            keychain.unlock()
+        }
+        btnToggleLocked.setText(if (keychain.locked) R.string.btn_locked_unlock else R.string.btn_locked_lock)
+        keyvalAdapter.notifyDataSetChanged()
     }
 }
